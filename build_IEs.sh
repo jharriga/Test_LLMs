83ea3225a89c1c0f4cdab0e4d2ac63ccf7620d5b
John Harrigan <jharriga@redhat.com>
2025-07-10 15:06:16 -0400

    Update build_IEs.sh

--- build_IEs.sh
+++ build_IEs.sh
@@ -23,9 +23,9 @@ echo "Done cloning the Inference Engine repos"
 
 echo "Build vLLM using 'podman build'"
 cd vllm_git
-podman build -f docker/Dockerfile.cpu \
---build-arg VLLM_CPU_DISABLE_AVX512="false" \
-     --tag vllm-cpu-env --target vllm-openai .
+podman build -f vllm/docker/Dockerfile.cpu \
+  --build-arg VLLM_CPU_DISABLE_AVX512="false" \
+  --tag vllm-cpu-env --target vllm-openai .
 
 echo "Build llama.cpp using 'cmake'"
 cd llama.cpp
