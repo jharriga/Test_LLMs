f6e0e7fcdb948c4d914aa3599652d02bca4a01b5
John Harrigan <jharriga@redhat.com>
2025-07-10 14:46:23 -0400

    Create build_IEs.sh

---
+++ build_IEs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Script which clones vLLM and llama.cpp repos and then builds them
+# To be used in comparison of vLLM and llama.cpp Inference Engines
+
+IErepo_arr=("https://github.com/ggml-org/llama.cpp"
+    "https://github.com/vllm-project/vllm")
+
+# Install necessary Tools
+# llama.cpp requires python and cmake
+# vLLM requires podman for build & run
+dnf install podman python3 cmake
+
+echo "Clone the Inference Engine repos"
+for IE_repo in "${IErepo_arr[@]}"; do
+    IE_name="$(basename "$IE_repo")"
+    IE_path="$PWD/$IE_name"
+    if [ ! -d "$IE_path" ]; then
+       echo "Cloning $IE_repo"
+       git clone "${IE_repo}"
+    fi
+done
+echo "Done cloning the Inference Engine repos"
+
+echo "Build vLLM using 'podman build'"
+cd vllm_git
+podman build -f docker/Dockerfile.cpu \
+--build-arg VLLM_CPU_DISABLE_AVX512="false" \
+     --tag vllm-cpu-env --target vllm-openai .
+
+echo "Build llama.cpp using 'cmake'"
+cd llama.cpp
+cmake -B build
+cmake --build build --config Release -j $(nproc)
+
+
